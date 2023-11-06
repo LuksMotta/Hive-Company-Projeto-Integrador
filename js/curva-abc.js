9ae70b360b284915c9e1eb694ff5e98084fded5c
WillSenai <wiliam.ribeiro@aluno.senai.br>
2023-11-06 10:52:15 -0300

    Co-authored-by: Ivann-s <Ivann-s@users.noreply.github.com> Co-authored-by: ivanilto78 <ivanilto78@users.noreply.github.com>

--- js/curva-abc.js
+++ js/curva-abc.js
@@ -1,13 +1,14 @@
-const vtci = document.querySelector('.vtci');
-const pa = document.querySelector('.percentual');
+const ValorTotalCadaItem = document.querySelector('.vtci');
+const percentual = document.querySelector('.percentual');
 const btn = document.querySelector('.btn')
 const qtd = document.querySelector('.qtd');
 const vendas = document.querySelector('.vendas');
 const preco = document.querySelector('.preco');
+let valorpercentual = 0
 
-
-btn.addEventListener('click', () => {
-    vtci.value = qtd.value * preco.value;
-    pa.value = (qtd.value * preco.value / qtd) * 100;
-    console.log(pa.value)
+btn.addEventListener('click', () => {    
+    ValorTotalCadaItem.value = qtd.value * preco.value;
+    valorpercentual = (qtd.value * preco.value / vendas.value) * 100
+    percentual.value = valorpercentual +"%";
+    console.log(percentual.value)
 })
